19d881619b867fda96a29bb077ea695151a32a22
vladooha <vladeeslavue@yandex.com>
2019-10-23 20:43:35 +0300

    Repo init

---
+++ src/main/java/com/vladooha/epilepsycenterserviceappbackend/EpilepsyCenterServiceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vladooha.epilepsycenterserviceappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpilepsyCenterServiceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpilepsyCenterServiceAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladooha/epilepsycenterserviceappbackend/EpilepsyCenterServiceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladooha.epilepsycenterserviceappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpilepsyCenterServiceAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
